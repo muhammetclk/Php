40e2c1ccafca18777a2e49e584f333f271d7d91d
Muhammet Çelik <muhammetclk56@gmail.com>
2023-05-03 00:51:12 +0300

    Aciklama satiri kullanimi yapildi.Echo ve print komutu kullanimi yapildi.Php etiket acilimi ve kapatilmasi yapildi.Html icinde php etiketi kullanildi.

---
+++ 3-PhpBaslangic/index.php
@@ -0,0 +1,24 @@
+<!--acıklama html -->
+<h1>Hello world for h1</h1>
+ 
+<?php
+//acıklama   echo ve print ile ekrana yazar.
+#acıklama satırı
+/*acıkalma satırı */
+echo 'Hello world for echo';
+
+echo 'Hello world for echo';
+print 'Hello world for print' /*burda ; kullanmadık ?> otomatik olarak getiriyor.*/
+?>
+
+<h1><?php echo 'hello world' ?></h1>
+<h1><?=  'hello world' ?></h1><!-- burdakı = echo anlamına gelıyor. h1 ıcınde php kullandık -->
+<h1><?php print 'hello world' ?></h1>
+
+
+<?php echo "Muhammet Celik";
+
+
+?>
+
+<h2><?php echo 'Hello world' ?></h2>
