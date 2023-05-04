71fab9739f4af8d470d88017618f5e0cd532ceaa
Muhammet Çelik <muhammetclk56@gmail.com>
2023-05-04 17:37:47 +0300

    degisken tanimlamasi yapildi.birlestirme islemi yapildi.

---
+++ 5-Degiskenler/degiskenler.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Değişkenler
+PHP'de değişkenler dolar işaretini($) takip eden bir değişken adı ile gösterilir.
+
+Değişken adı büyük-küçük harf duyarlıdır.
+Türkçe karakter içerebilir.
+Tüm veri tiplerini tutabilir.
+Harf ve alt çizgi ile başlar.
+Atama operatörü(=) ile değer ataması yapılır.
+*/ 
+
+$isim="Muhammet ÇElik";
+echo $isim;
+$isim="aliveli";// degiskenin degerini degistirebiliriz.
+echo $isim;
+
+$a=5;
+$A=4;
+echo $a;
+echo "<br>";
+echo $A;
+$sayi1=5;
+$sayi2=6;
+$carpim=$sayi1*$sayi2;
+echo 'sayi1 ve sayi2 toplam:'. $sayi1+$sayi2;
+echo '<br>';
+echo 'sayi1 ve sayi2 carpimi:'.$carpim;
+
+
