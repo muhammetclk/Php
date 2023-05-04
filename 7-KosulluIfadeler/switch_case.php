02537986ffcda90600d2917f87f976701aa0f72e
Muhammet Çelik <muhammetclk56@gmail.com>
2023-05-05 00:02:02 +0300

    switch case kullanimi yapildi.

---
+++ 7-KosulluIfadeler/switch_case.php
@@ -0,0 +1,27 @@
+<?php
+
+$a=5;
+switch($a){
+
+case $a==1:
+    echo $a.' esittir 1.kosul';
+    break;
+case $a==2:
+    echo $a.' esittir 2.kosul';
+    break;
+case $a==3:
+    echo $a.' esittir 3.kosul';
+    break;
+case $a==4:
+    echo $a.' esittir 4.kosul';
+    break;
+case $a==5:
+    echo $a.' esittir 5.kosul';
+    break;
+default:
+echo $a.'hic bir kosula uymadı';
+break;
+
+            
+        
+}
