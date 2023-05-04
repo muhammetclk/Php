6dd22ed692eeaad541a41a5fa396d5b493bfd4c1
Muhammet Çelik <muhammetclk56@gmail.com>
2023-05-04 23:21:08 +0300

    Aritmetik operatorlerle ilgili islemler yapildi.

---
+++ 6-Operatorler/AritmetikOperatorler.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Aritmetik Operatörler
+Operatörler
+Toplama (+)
+Çıkartma (–)
+Çarpma (*)
+Bölme (/)
+Mod (%)
+Aritmetik operatörleri kullanırken matematikte olduğu gibi işlem sırası vardır. Önce Parantez içleri, çarpma/bölme ve toplama çıkarma işlemleri sırasıyla uygulanır.
+ */
+
+echo 7+2;
+echo "<br>";
+echo 7-2;
+echo "<br>";
+echo 6/3;
+echo "<br>";
+echo 6*3;
+echo "<br>";
+echo 6%2;
+echo "<br>";
+
+$sayi1=10;//degerleri degiskene atayip sonucu yazdırdık
+$sayi2=20;
+echo "<br>";
+echo $sayi1+$sayi2;
+echo "<br>";
+$sayi3=$sayi1+$sayi2;//degisknelerin degerini toplatip baska bir degsikene atadik.
+echo $sayi3;
+echo "<br>";
+echo $sayi3/3;//degiskeni sayi ile bolduk.
